7840ab53321ad0e1e7de47de5dcb5ac567ff433c
wes-mitchell <wmdrums@gmail.com>
2022-03-24 20:27:26 -0500

    adds black and white photos and applies css wrap and justify content

--- imageList.js
+++ imageList.js
@@ -1,13 +1,15 @@
 import { getImages } from "./imageData.js";
 import { imageHTML } from "./image.js";
 
+// Grabs all images from json and populates DOM with images.
+
 export const imageList = () => { 
   const imageEl = document.querySelector(".content-container")
-  const images = getImages()
-  let imageHTMLRep = '';
-
-  for (const image of images) { 
-    imageHTMLRep += imageHTML(image)
-   }
-   imageEl.innerHTML +=`${imageHTMLRep}`
- }
+  let imageHTMLRep = ''
+  getImages().then(allImages => {
+    for (const image of allImages) { 
+      imageHTMLRep += imageHTML(image)
+     }
+     imageEl.innerHTML +=`${imageHTMLRep}`
+  })
+}
