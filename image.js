e16e545a07fb0c2bd37abd74486209ca4862a09d
wes-mitchell <wmdrums@gmail.com>
2022-04-13 19:17:11 -0500

    wraps image in anchor tag for re-route

--- image.js
+++ image.js
@@ -2,7 +2,7 @@ export const imageHTML = (image) => {
   return `<div class="image-card">
   <div class="image">
   <h2 class="title">${image.title}</h2>
-  <img src="${image.url}" alt="">
+  <a href="${image.raw}" class="test" target="_blank"><img src="${image.url}" alt=""></a>
   <p class="year">${image.location} - ${image.year}</p>
   <hr>
   <p class="image-description">${image.description}</p>
--- imageData.js
+++ imageData.js
@@ -7,6 +7,34 @@ export const getImages = () => {
     })
 }
 
+
+// export const getImageById = (imageId) => { 
+//   return fetch(`https://wes-mitchell.github.io/photoapi/database.json/`)
+//   .then(response => response.json())
+//   .then(allData  => {
+//     // console.log(allData.images);
+//     allData.images.filter(image => {
+//       // console.log(image);
+//       const filteredImage = []
+//       if (image.id === imageId) {
+//       filteredImage.push(image)
+//       return 
+//       }
+//       return filteredImage
+//   })
+//   }    
+// )}
+
+
+
+
+
+// export const showRaw = (imageArr) => {
+//   console.log(imageArr); 
+//   window.open(`${imageArr.raw}`, "_blank")
+//  }
+
+
 // export const getImages = () => {
 //     return fetch("http://localhost:8088/images")
 //       .then(response => response.json())
